4a6360441f9d1342c6ee46fe7fd973f01a3c1dbd
jmphilips <jmichaelphilips@gmail.com>
2018-09-19 16:57:11 -0400

    Removed camera logic from timer logic.

--- controllers/countdown.js
+++ controllers/countdown.js
@@ -5,14 +5,14 @@ const { Raspistill } = require('node-raspistill');
 module.exports.index = (req, res) => {
   let count = 4;
   const myCamera = createCamera();
+  createCamera(count)
+    .timelapse('image%04d', 5000, 2000)
+    .then(image => console.log(`image taken ${image}`))
+    .catch(err => console.log(err));
   const myTimer = setInterval(() => {
     if (count === 0) {
       clearInterval(myTimer);
     }
-    createCamera(count)
-      .timelapse('image%04d', 5000, 2000)
-      .then(image => console.log(`image taken ${image}`))
-      .catch(err => console.log(err));
     count -= 1;
   }, 5000);
   res.render('countdown');
@@ -21,6 +21,6 @@ module.exports.index = (req, res) => {
 const createCamera = () => {
   return new Raspistill({
     height: 400,
-    width: 400,
+    width: 400
   });
 };
